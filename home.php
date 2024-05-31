ada4615dea219a227a0906fa4a66bb7be68298d2
Manish Dahal <dahalmanish9@gmail.com>
2024-05-31 07:27:27 +0545

    Sixth Commit

---
+++ home.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<style>
+   #footer {
+width: 100%;
+height: 75px;
+background-color:#000000;
+color:white;
+text-align: center;
+}
+</style>
+</head>
+
+<body>
+<div class="header">
+<?php
+$active="home";
+include('head.php'); ?>
+
+</div>
+
+  <div id="page-container" style="margin-top:50px; position: relative;min-height: 84vh;   ">
+    <div class="container">
+    <div id="content-wrap"style="padding-bottom:75px;">
+  <div id="demo" class="carousel slide" data-ride="carousel">
+
+    <!-- Indicators -->
+    <ul class="carousel-indicators">
+      <li data-target="#demo" data-slide-to="0" class="active"></li>
+      <li data-target="#demo" data-slide-to="1"></li>
+    </ul>
+
+    <!-- The slideshow -->
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="image\_107317099_blooddonor976.jpg" alt="image\_107317099_blooddonor976.jpg" width="100%" height="500">
+      </div>
+      <div class="carousel-item">
+        <img src="image\Blood-facts_10-illustration-graphics__canteen.png" alt="image\Blood-facts_10-illustration-graphics__canteen.png" width="100%" height="500">
+      </div>
+
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="carousel-control-prev" href="#demo" data-slide="prev">
+      <span class="carousel-control-prev-icon"></span>
+    </a>
+    <a class="carousel-control-next" href="#demo" data-slide="next">
+      <span class="carousel-control-next-icon"></span>
+    </a>
+  </div>
+<br>
+        <h1 style="text-align:center;font-size:45px;">Welcome to BloodBank & Donor Management System</h1>
+<br>
+        
+</div>
+
+        <div class="row">
+</div>
+<br>
+        <!-- /.row -->
+
+        <!-- Features Section -->
+        <div class="row">
+            <div class="col-lg-6">
+                <h2>BLOOD GROUPS</h2>
+                <p>
+                <span style="color: rgb(0, 0, 0); font-family:Open Sans, Arial, sans-serif; text-align: justify;">
+  <p>  Blood group of any human being will mainly fall in any one of the following groups.</p>
+                <ul>
+
+                  <li>A positive or A negative</li>
+                  <li>B positive or B negative</li>
+                  <li>O positive or O negative</li>
+                  <li>AB positive or AB negative.</li>
+                </ul>
+                <p>Your blood group is determined by the genes you inherit from your parents.<br>
+                  A healthy diet helps ensure a successful blood donation, and also makes you feel better!
+                </p>
+
+</span></p>
+
+            </div>
+            <div class="col-lg-6">
+                <img class="img-fluid rounded" src="image\blood_donationcover.jpeg" alt="" >
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <!-- Call to Action Section -->
+        <div class="row mb-4">
+            <div class="col-md-8">
+            <h4>UNIVERSAL DONORS AND RECIPIENTS</h4>
+            <p>
+            <span style="color: rgb(0, 0, 0); font-family: Open Sans, Arial, sans-serif; text-align: justify;">
+  <p>
+The most common blood type is O, followed by type A.
+
+Type O individuals are often called "universal donors" since their blood can be transfused into persons with any blood type. Those with type AB blood are called "universal recipients" because they can receive blood of any type.</p>
+
+            For emergency transfusions, blood group type O negative blood is the variety of blood that has the lowest risk of causing serious reactions for most people who receive it. Because of this, it's sometimes called the universal blood donor type.
+
+</span></p>
+              </div>
+            <div class="col-md-4">
+                <a class="btn btn-lg btn-secondary btn-block" href="donate_blood.php" style="align-items:center; background-color:#7FB3D5;color:#273746 ">Become a Donor </a>
+            </div>
+        </div>
+
+    </div>
+  </div>
+</div>
+<div>
+<div id="footer" >
+  <b><center>COPYRIGHT © 2020<br>
+  Blood Bank & Donation Management
+  <br>
+  ALL RIGHTS RESERVED.
+  </center>
+  </div>
+</div>
+</body>
+
+</html>
+
---
+++ why_donate_blood.php
@@ -0,0 +1,52 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+
+
+<?php 
+$active ='why';
+include('head.php');
+?>
+
+<div id="page-container" style="margin-top:50px; position: relative;min-height: 84vh;">
+  <div class="container">
+  <div id="content-wrap" style="padding-bottom:50px;">
+<div class="row">
+    <div class="col-lg-6">
+        <h1 class="mt-4 mb-3">Why Should I Donate Blood ? </h1>
+        <p>
+        <span style="color: rgb(0, 0, 0); font-family: Open Sans, Arial, sans-serif; font-size: 14px; text-align: justify;">Blood is the most precious gift that anyone can give to another person — the gift of life. 
+        A decision to donate your blood can save a life, or even several if your blood is separated into its components — red cells, platelets and plasma — which can be used individually for patients with specific conditions. Safe blood saves lives and improves health. Blood transfusion is needed for:
+<ul><li>women with complications of pregnancy, such as ectopic pregnancies and haemorrhage before, during or after childbirth.
+</li><li>children with severe anaemia often resulting from malaria or malnutrition.
+</li><li>people with severe trauma following man-made and natural disasters.
+</li><li>many complex medical and surgical procedures and cancer patients.</li></ul>
+<br>It is also needed for regular transfusions for people with conditions such as thalassaemia and sickle cell disease and is used to make products such as clotting factors for people with haemophilia. There is a constant need for regular blood supply because blood can be stored for only a limited time before use. Regular blood donations by a sufficient number of healthy people are needed to ensure that safe blood will be available whenever and wherever it is needed.</span>
+      </p>
+
+    </div>
+    <div class="col-lg-6">
+        <img class="img-fluid rounded" src="image\08f2fccc45d2564f74ead4a6d5086871.png" style="height:600px; width:500px" alt="error"  >
+    </div>
+</div>
+</div>
+
+</div>
+
+<?php include('footer.php')
+?>
+</div>
+</body>
+
+</html>
